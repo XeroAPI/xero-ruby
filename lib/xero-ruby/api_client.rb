d6cd634efa3d6831be1872e4c42e11d8f226541d
serknight <chrisknight.mail@gmail.com>
2021-08-04 14:14:02 -0600

    deserialization and readme typo fix

--- lib/xero-ruby/api_client.rb
+++ lib/xero-ruby/api_client.rb
@@ -506,6 +506,8 @@ module XeroRuby
           XeroRuby::PayrollNz.const_get(return_type).build_from_hash(data)
         when 'PayrollUkApi'
           XeroRuby::PayrollUk.const_get(return_type).build_from_hash(data)
+        when 'AppStoreApi'
+          XeroRuby::AppStoreApi.const_get(return_type).build_from_hash(data)
         else
           XeroRuby::Accounting.const_get(return_type).build_from_hash(data)
         end
